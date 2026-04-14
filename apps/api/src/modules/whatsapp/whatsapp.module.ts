2a15bdb367aa2704364aa21eaab1e2d415d47852
vingignoli-commits <vingignoli@gmail.com>
2026-04-14 13:51:38 -0300

    Add PrismaModule to WhatsappModule imports

--- apps/api/src/modules/whatsapp/whatsapp.module.ts
+++ apps/api/src/modules/whatsapp/whatsapp.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuditModule } from '../audit/audit.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { WhatsappController } from './whatsapp.controller';
 import { WhatsappService } from './whatsapp.service';
 
 @Module({
-  imports: [AuditModule],
+  imports: [PrismaModule, AuditModule],
   controllers: [WhatsappController],
   providers: [WhatsappService],
 })
 export class WhatsappModule {}
-
